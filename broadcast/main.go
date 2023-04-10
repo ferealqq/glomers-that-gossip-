5fa4e2994c7fe90dcc2ed32ffd3ebdbbbfb670d5
Pekka Mattinen <pekka.mattinen@webso.fi>
2023-04-10 16:02:34 +0300

    merged broadcast dirs

--- broadcast/main.go
+++ broadcast/main.go
@@ -7,15 +7,32 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+//str => destination
+//any => body
+type FailedMsg = map[string]any
+
 func main() {
 	n := maelstrom.NewNode()
 	lst := []int{}
+	failChan := make(chan FailedMsg)
+	// handle failed messages
+	go func ()  {
+		// try to resend failed messages 
+		for msg := range failChan {
+			for dst, body := range msg {
+				n.Send(dst,body)
+			}
+		}
+	}()
+
+	// TODO Nodes should only return copies of their own local values?
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+		// try to unpack message
 		switch body["message"].(type) {
 		case float64:
 			lst = append(lst, int(body["message"].(float64)))
@@ -25,6 +42,17 @@ func main() {
 			return n.Reply(msg, map[string]string{
 				"type": "error",
 			})
+		}
+		// push failed messages to failed channel which will try to resend failed messages
+		failed := make(FailedMsg)
+		for _,i := range n.NodeIDs() {
+			if i == n.ID() {continue}
+			if err := n.Send(i, body); err != nil {
+				failed[i] = body
+			}
+		}
+		if len(failed) > 0 {
+			failChan <- failed			
 		}
 		res := map[string]string{
 			"type": "broadcast_ok",
